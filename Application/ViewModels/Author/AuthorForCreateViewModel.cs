df4a1a8be36bda32d75637fbd26729e44e04a588
Ali Ilci <Ali.Ilci@gmx.de>
2026-09-25 10:28:09 +0200

    NewAuthor

---
+++ Application/ViewModels/Author/AuthorForCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Application.ViewModels.Author;
+
+public class AuthorForCreateViewModel
+{
+    public Guid Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Description { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public IBrowserFile? Photo { get; set; }
+}
---
+++ UI.Blazor/Components/Pages/Author/AuthorNew.razor.cs
@@ -0,0 +1,8 @@
+
+
+namespace UI.Blazor.Components.Pages.Author;
+
+public partial class AuthorNew
+{
+    
+}
